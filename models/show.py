13c4a1ce0844b9cd86aa4ff338b5a77ce6601ecc
Christopher Wall <wallandall@gmail.co>
2021-05-29 20:12:26 +0200

    Updated Show Model

--- models/show.py
+++ models/show.py
@@ -4,14 +4,10 @@ from db import db
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey(
         'Artist.id'), nullable=False)
-    artist = db.relationship(
-        'Artist', backref=db.backref('shows', cascade='all, delete'))
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    venue = db.relationship(
-        'Venue', backref=db.backref('shows', cascade='all, delete'))
-    start_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return '<Show {}{}>'.format(self.artist_id, self.venue_id)
