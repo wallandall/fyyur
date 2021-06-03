60a8c8ef93bd7a44910d6ef28f56216dde9e1ea8
Christopher Wall <wallandall@gmail.co>
2021-06-03 14:55:42 +0200

    Updated data type for genre Venue Model

--- models.py
+++ models.py
@@ -17,8 +17,7 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    #genres = db.Column(db.ARRAY(db.String(120)))
-    genres = db.Column(db.String)
+    genres = db.Column(db.String(120))
     address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
@@ -28,8 +27,6 @@ class Venue(db.Model):
     seeking_talent = db.Column(db.Boolean, default=True)
     seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
-    # shows = db.relationship('Show', backref='Venue',
-    #                        lazy=True, cascade='all, delete-orphan')
 
     @ classmethod
     def find_by_name(cls, search_term):
